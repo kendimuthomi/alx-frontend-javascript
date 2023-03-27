cfc5c216f5af4b4fb0d5b36cabcc62882ef40d6f
kendimuthomi <kendijackline42@gmail.com>
2023-03-27 10:34:05 +0300

    Checking that lint tests are passing

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,4 +1,4 @@
-export default function getSanFransiscoDescription () {
+export default function getSanFransiscoDescription() {
   const year = 2017;
   const budget = {
     income: '$119,868',
@@ -7,5 +7,5 @@ export default function getSanFransiscoDescription () {
   };
   return `As of ${year}, it was the seventh-highest income county in the United States,\
   with a per capita personal income of ${budget.income}. As of 2015, San Fransisco\
-  proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`
+  proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
