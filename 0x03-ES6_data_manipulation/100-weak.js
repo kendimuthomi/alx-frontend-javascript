e01663dd4394fa979b2066f39afedf4a317987c0
kendimuthomi <kendijackline42@gmail.com>
2023-03-28 12:18:17 +0300

    Task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export default function queryAPI(endpoint) {
   let count = weakMap.get(endpoint) || 0;
   count += 1;
 
-  if (count > 5) {
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endpoint, count);
