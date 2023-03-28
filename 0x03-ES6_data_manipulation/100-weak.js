bbe213ea9fa6697ad4d4373b261d51d19fc66a04
kendimuthomi <kendijackline42@gmail.com>
2023-03-28 12:22:14 +0300

    Task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,14 @@
 export const weakMap = new WeakMap();
 export default function queryAPI(endpoint) {
-  let count = weakMap.get(endpoint) || 0;
+  const { protocol, name } = endpoint;
+  const key = `${protocol}:${name}`;
+
+  let count = weakMap.get(key) || 0;
   count += 1;
 
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   } else {
-    weakMap.set(endpoint, count);
+    weakMap.set(key, count);
   }
 }
