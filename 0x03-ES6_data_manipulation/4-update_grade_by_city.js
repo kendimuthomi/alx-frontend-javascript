951a53b6f192db152b392b8ff76da011c959c2a0
kendimuthomi <kendijackline42@gmail.com>
2023-03-28 12:35:19 +0300

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,3 @@
 export default function updateStudentGradeByCity(students, city, newGrade) {
-  const studentsInCity = students.filter(student => student.location === city);
-  const updatedStudents = studentsInCity.map(student => {
-    const grade = newGrade.find(grade => grade.id === student.id);
-    if (grade) {
-      return {
-        ...student,
-        grade: grade.grade,
-      };
-    } else {
-      return {
-        ...student,
-        grade: 'N/A',
-      };
-    }
-  });
-  return updatedStudents;
+  return students.filter((student) => student.location === city).map((student) => ({ ...student, grade: newGrade.filter((grad) => grad.studentId === student.id).map((grad) => grad.grade)[0] || 'N/A' }));
 }
