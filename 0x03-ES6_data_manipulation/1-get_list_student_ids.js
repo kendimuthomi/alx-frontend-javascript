695a79845f49c9e8943bd41f3e83d9cfcafa0511
kendimuthomi <kendijackline42@gmail.com>
2023-03-28 08:34:48 +0300

    Task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+const arr = [
+    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    {id: 2, firstName: 'James', location: 'Columbia'},
+    {id: 5, firstName: 'Serena', location: 'San Francisco'},
+  ];
+export default function getListStudentIds(arr) {
+  return arr.map(arr => arr.id);
+}
