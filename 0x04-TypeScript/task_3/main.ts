383e8e2bc89783d4fcb9f8bbcba28d6faf2adf1f
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-03-29 18:32:08 +0300

    Task  3

--- 0x04-TypeScript/task_3/main.ts
+++ 0x04-TypeScript/task_3/main.ts
@@ -1,45 +1,15 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
-}
+/// <reference path="./crud.d.ts" />
 
-const student1: Student = {
-  firstName: "Nicole",
-  lastName: "Mwenda",
-  age: 2,
-  location: "Marsabit"
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
 };
 
-const student2: Student = {
-  firstName: "Ryan",
-  lastName: "Mwenda",
-  age: 4,
-  location: "Meru"
-};
+const newRowID: RowID = CRUD.insertRow(row);
+const UpdatedRow: RowElement = { age: 23, ...row };
 
-const studentsList: Student[] = [student1, student2];
-
-const table = document.createElement("table");
-const headerRow = document.createElement("tr");
-const firstNameHeader = document.createElement("th");
-firstNameHeader.innerText = "First Name";
-const locationHeader = document.createElement("th");
-locationHeader.innerText = "Location";
-headerRow.appendChild(firstNameHeader);
-headerRow.appendChild(locationHeader);
-table.appendChild(headerRow);
-
-studentsList.forEach(student => {
-  const row = document.createElement("tr");
-  const firstNameCell = document.createElement("td");
-  firstNameCell.innerText = student.firstName;
-  const locationCell = document.createElement("td");
-  locationCell.innerText = student.location;
-  row.appendChild(firstNameCell);
-  row.appendChild(locationCell);
-  table.appendChild(row);
-});
-
-document.body.appendChild(table);
+CRUD.updateRow(newRowID, UpdatedRow);
+CRUD.deleteRow(newRowID);
