a69bfb21c30688b7be95f073f85cbd450ad7f7d4
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-03-29 18:39:11 +0300

    Task 5

--- 0x04-TypeScript/task_5/main.ts
+++ 0x04-TypeScript/task_5/main.ts
@@ -1,45 +1,17 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+interface MajorCredits {
+  credits: number;
+  brand: "major";
 }
 
-const student1: Student = {
-  firstName: "Nicole",
-  lastName: "Mwenda",
-  age: 2,
-  location: "Marsabit"
-};
+interface MinorCredits {
+  credits: number;
+  brand: "minor";
+}
 
-const student2: Student = {
-  firstName: "Ryan",
-  lastName: "Mwenda",
-  age: 4,
-  location: "Meru"
-};
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+  return subject1.credits + subject2.credits;
+}
 
-const studentsList: Student[] = [student1, student2];
-
-const table = document.createElement("table");
-const headerRow = document.createElement("tr");
-const firstNameHeader = document.createElement("th");
-firstNameHeader.innerText = "First Name";
-const locationHeader = document.createElement("th");
-locationHeader.innerText = "Location";
-headerRow.appendChild(firstNameHeader);
-headerRow.appendChild(locationHeader);
-table.appendChild(headerRow);
-
-studentsList.forEach(student => {
-  const row = document.createElement("tr");
-  const firstNameCell = document.createElement("td");
-  firstNameCell.innerText = student.firstName;
-  const locationCell = document.createElement("td");
-  locationCell.innerText = student.location;
-  row.appendChild(firstNameCell);
-  row.appendChild(locationCell);
-  table.appendChild(row);
-});
-
-document.body.appendChild(table);
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+  return subject1.credits + subject2.credits;
+}
